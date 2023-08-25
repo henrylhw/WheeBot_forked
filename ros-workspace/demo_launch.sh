dba7ac5a0ab4431d4b046aae7d18ca44154b10ac
Henry <henrylhw@connect.hku.hk>
2023-08-25 16:20:08 +0800

    improved packaging and added hector SLAM

---
+++ ros-workspace/demo_launch.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a comment!
+ 
+sudo chmod 666 /dev/ttyTHS1
+sudo chmod 666 /dev/ttyUSB0
+# catkin_make
+source devel/setup.bash
+roscd
+cd ..
+roslaunch fake_intelligence_bringup slam_fpv_teleop.launch
+echo Live-stream video: http://192.168.99.120:8080/stream_viewer?topic=/usb_cam_node/image_raw
